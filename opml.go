4aded45f76d98cd7f3d16a592cd4642bd5c558a6
hikari <ftphikari@gmail.com>
2022-01-18 11:59:11 +0200

    remove potential tags for now

--- opml.go
+++ opml.go
@@ -8,10 +8,9 @@ import (
 )
 
 type Outline struct {
-	Outlines []Outline `xml:"outline",omitempty`
-	Title    string    `xml:"title,attr,omitempty"`
-	Text     string    `xml:"text,attr,omitempty"`
-	XmlUrl   string    `xml:"xmlUrl,attr"`
+	Title  string `xml:"title,attr,omitempty"`
+	Text   string `xml:"text,attr,omitempty"`
+	XmlUrl string `xml:"xmlUrl,attr"`
 }
 
 type OPML struct {
@@ -25,14 +24,12 @@ func addOutlines(out []Outline) {
 	for _, o := range out {
 		var f Feed
 		f.Title = "?"
-		if len(o.Outlines) > 0 {
-			addOutlines(o.Outlines)
-			continue
-		}
 		if len(o.Title) > 0 {
 			f.Title = o.Title
 		} else if len(o.Text) > 0 {
 			f.Title = o.Text
+		} else {
+			f.Title = "?"
 		}
 		if len(o.XmlUrl) > 0 {
 			f.Url = o.XmlUrl
@@ -60,7 +57,7 @@ func importOPML(data []byte) error {
 func exportOPML(f string) error {
 	var o OPML
 	o.Version = "2.0"
-	o.Title = "OPML export from GROSS"
+	o.Title = "OPML export from GRoSS"
 	for _, f := range feeds {
 		var out Outline
 		out.Title = f.Title
